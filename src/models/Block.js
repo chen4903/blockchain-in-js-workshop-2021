506bca4c545b9af38da3ad10fd5b5ed5f812343a
chenqin <490358423@qq.com>
2023-05-17 21:58:55 +0800

    lesson2 completely_update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,3 +1,5 @@
+import sha256 from 'crypto-js/sha256.js'
+
 export const DIFFICULTY = 3
 
 class Block {
@@ -7,51 +9,23 @@ class Block {
     this.previousHash = previousHash
     this.height = height
     this.hash = hash
-    this.nonce = 0 // 此版本是找到前n位是0的hash值
-    //this.nonce = "" // 此版本是找到前n位是0的nonce值
+    this.nonce = 0
   }
 
-/*
-###########################################################
-                此版本是找到前n位是0的nonce值
-###########################################################
-
-    这里是设置随机数，而不是设置hash
-    如果符合区块链逻辑的话，应该是设置hash的前n位是0，这个版本已经写了，在前面的注释当中
-    这个版本更容易跑通测试
-
-  isValid() {
-    if (this.nonce.startsWith('0'.repeat(DIFFICULTY))) {
-      return true
-    }else {
-      return false
-    }
-  }
-
-  setNonce(nonce) { 
-    this.nonce = nonce
-  }
-*/  
-
-
-/*
-
-###########################################################
-                此版本是找到前n位是0的hash值
-###########################################################
-
-    此版本会触发内存溢出，很奇怪
-    只有尝试很多次才成功一次，可能存储的区块数据过多，导致内存溢出了
-
-    ！！！！！！如果多次尝试无法成功，请尝试上面注释的版本！！！！！！！
-*/
   isValid() {
     const prefix = "0".repeat(DIFFICULTY);
+    if(this.hash.substring(0, DIFFICULTY) === prefix){
+      console.log("blockHash:",this.hash)
+    }
     return this.hash.substring(0, DIFFICULTY) === prefix;
   }
 
-  setNonce(_hash) {
-    this.hash = _hash
+  setNonce(_time) {
+    // 因为传入的是时间戳，计算机在一秒当中可以运行很多次，尝试很多次，但是结果都是一致的(时间戳不变)
+    // 因此我们需要将时间戳加上nonce，然后sha256，就可以保证在一秒中计算出来的结果不一样
+    // 不这样做的话，会非常耗费系统资源，导致内存溢出。当然此处也经常出现内存溢出的情况
+    let temp = parseInt(_time) + this.nonce
+    this.hash = sha256(temp.toString()).toString()
     this.nonce++
   }
 }
