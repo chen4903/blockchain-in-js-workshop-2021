a46a2c178f216cc0de3585d9e928d2c533d0ef04
chenqin <490358423@qq.com>
2023-06-02 21:56:44 +0800

    complete lesson5

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,25 +1,48 @@
 import sha256 from 'crypto-js/sha256.js'
+import UTXO from './UTXO.js'
+import { all } from 'ramda'
 
-export const DIFFICULTY = 2
+export const DIFFICULTY = 3
 
 class Block {
-  // 1. 完成构造函数及其参数
 
-  constructor() {}
+  constructor(_blockchain, _previousHash, _height, _hash, _coinbaseBeneficiary) {
+    this.blockchain = _blockchain
+    this.previousHash = _previousHash
+    this.height = _height
+    this.hash = _hash
+    this.nonce = 0
+    this.coinbaseBeneficiary = _coinbaseBeneficiary
+    this.utxoPool = null
+  }
 
-  isValid() {}
+  isValid() {
+    const prefix = "0".repeat(DIFFICULTY);
+    return this.hash.substring(0, DIFFICULTY) === prefix
+  }
 
-  setNonce(nonce) {}
-
-  // 根据交易变化更新区块 hash
-  _setHash() {}
+  setNonce(_time) {
+    // 因为传入的是时间戳，计算机在一秒当中可以运行很多次，尝试很多次，但是结果都是一致的(时间戳不变)
+    // 因此我们需要将时间戳加上nonce，然后sha256，就可以保证在一秒中计算出来的结果不一样
+    // 不这样做的话，会非常耗费系统资源，导致内存溢出。当然此处也经常出现内存溢出的情况
+    let temp = parseInt(_time) + this.nonce
+    this.hash = sha256(temp.toString()).toString()
+    this.nonce++
+  }
 
   // 汇总计算交易的 Hash 值
   /**
    * 默克尔树实现
    */
-  combinedTransactionsHash() {
-
+  combinedTransactionsHash(){
+    var allTransaction = ""
+    const keys = Object.keys(this.utxoPool.utxos);
+    for(let i = 0;i < keys.length; i++){
+      allTransaction = allTransaction+ keys[i] + this.utxoPool.utxos[keys[i]].amount.toString()
+      allTransaction = sha256(allTransaction).toString()
+    }
+    this.hash = sha256(allTransaction).toString()
+    return this.hash
   }
 
   // 添加交易到区块
@@ -27,8 +50,29 @@ class Block {
    * 
    * 需包含 UTXOPool 的更新与 hash 的更新
    */
-  addTransaction() {}
-
+  addTransaction(trx) {
+    if(!this.utxoPool.isValidTransaction(trx.from,trx.value)){
+      // 失败的交易也打包上链，但是不合法，仅更新区块的哈希
+      // 失败的交易会更新一次failTx的amount，其值是sha256加密之后的结果
+      if(this.utxoPool.utxos["failTx"] == undefined){
+        let utxo = new UTXO()
+        this.utxoPool.utxos["failTx"] = utxo
+        this.utxoPool.utxos["failTx"].amount = sha256(this.utxoPool.utxos["failTx"].amount + trx.from + trx.to + trx.value).toString()
+      }else{
+        this.utxoPool.utxos["failTx"].amount = sha256(this.utxoPool.utxos["failTx"].amount + trx.from + trx.to + trx.value).toString()
+      }
+      return
+    }
+    this.utxoPool.utxos[trx.from].amount -= trx.value
+    if(this.utxoPool.utxos[trx.to] == undefined){
+        let utxo = new UTXO()
+        this.utxoPool.utxos[trx.to] = utxo
+        this.utxoPool.utxos[trx.to].amount += trx.value
+    }else{
+      this.utxoPool.utxos[trx.to].amount += trx.value
+    }
+    this.combinedTransactionsHash()
+  }
 }
 
 export default Block
